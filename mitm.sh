d0ea05c1c7b2b0b3056071614de49a819d73c50d
Vadim Yanushkevich <vadik.ya@gmail.com>
2018-02-23 11:01:18 +0200

    remove host parameter

--- mitm.sh
+++ mitm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOCKER="docker run --rm -it -v `pwd`/.mitmproxy:/home/mitmproxy/.mitmproxy -v `pwd`:/app -p 8080:8080 -w /app/workdir mitmproxy/mitmproxy"
-PARAMS=' --host -s /app/script/entry.py'
+PARAMS=' -s /app/script/entry.py'
 
 FULL="${DOCKER} mitmproxy ${PARAMS}"
 echo ${FULL}
