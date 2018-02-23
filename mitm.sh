6d8f5c7eb3c592f7f5edd733773ebb774768bd0f
Vadim Yanushkevich <vadik.ya@gmail.com>
2018-02-23 11:12:52 +0200

    update to mitmroxy 3.0.0

--- mitm.sh
+++ mitm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER="docker run --rm -it -v `pwd`/.mitmproxy:/home/mitmproxy/.mitmproxy -v `pwd`:/app -p 8080:8080 -w /app/workdir mitmproxy/mitmproxy"
+DOCKER="docker run --rm -it -v `pwd`/.mitmproxy:/home/mitmproxy/.mitmproxy -v `pwd`:/app -p 8080:8080 -w /app/workdir mitmproxy/mitmproxy:3.0.0"
 PARAMS=' -s /app/script/entry.py'
 
 FULL="${DOCKER} mitmproxy ${PARAMS}"
