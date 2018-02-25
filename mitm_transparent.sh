48f0947724d5c04cfe1a2790734859a13a7309bd
Vadim Yanushkevich <vadik.ya@gmail.com>
2018-02-25 23:33:39 +0200

    split usecases

--- mitm_transparent.sh
+++ mitm_transparent.sh
@@ -1,12 +1,10 @@
 docker run --rm -it \
       -w /app/workdir \
-      -p 8080:8080 \
+      -network host \
       -v `pwd`/.mitmproxy:/home/mitmproxy/.mitmproxy \
       -v `pwd`:/app \
       mitmproxy/mitmproxy:3.0.0 \
       mitmproxy \
       --anticache --ssl-insecure --showhost -p 8080 \
+      --mode transparent \
       -s /app/script/entry.py
-
-#NETWORK="--network host"
-#TRANSPARENT='--mode transparent'
